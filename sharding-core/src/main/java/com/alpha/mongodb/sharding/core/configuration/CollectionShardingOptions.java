757bed0bad144d5300b0719e7e22c8d4f762dc47
shashankrnr32 <shashankrnr32@gmail.com>
2023-03-31 23:38:27 +0530

    Add some more methods for Sharding Options

--- sharding-core/src/main/java/com/alpha/mongodb/sharding/core/configuration/CollectionShardingOptions.java
+++ sharding-core/src/main/java/com/alpha/mongodb/sharding/core/configuration/CollectionShardingOptions.java
@@ -1,10 +1,16 @@
 package com.alpha.mongodb.sharding.core.configuration;
 
 import lombok.Data;
+import lombok.ToString;
+import org.apache.commons.collections.CollectionUtils;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Data
+@ToString(callSuper = true)
 public class CollectionShardingOptions extends ShardingOptions {
     private List<String> collectionHints;
 
@@ -16,5 +22,12 @@ public class CollectionShardingOptions extends ShardingOptions {
         } else {
             return defaultCollectionHint;
         }
+    }
+
+    public void setIntStreamHints(IntStream stream) {
+        if (CollectionUtils.isEmpty(collectionHints)) {
+            collectionHints = new ArrayList<>();
+        }
+        this.collectionHints.addAll(stream.mapToObj(String::valueOf).collect(Collectors.toList()));
     }
 }
