19382391beb1faf55c269c1fda708883570bfea1
shashankrnr32 <shashankrnr32@gmail.com>
2023-04-09 09:22:33 +0530

    Update Documentation

--- sharding-spring/src/main/java/com/alpha/mongodb/sharding/core/ShardedReactiveMongoTemplate.java
+++ sharding-spring/src/main/java/com/alpha/mongodb/sharding/core/ShardedReactiveMongoTemplate.java
@@ -16,7 +16,7 @@ import java.util.Set;
 import java.util.function.Consumer;
 
 /**
- * Abstract Base Sharded Mongo Template
+ * Abstract Base Sharded Reactive Mongo Template
  *
  * @author Shashank Sharma
  */
